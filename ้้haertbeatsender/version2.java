d6d885b0d8bad61bb3dad86d550cfdebf0162cc0
660710616 <139957396+660710616@users.noreply.github.com>
2025-09-24 23:42:37 +0700

    Create version2.java

---
+++ ้้haertbeatsender/version2.java
@@ -0,0 +1,33 @@
+private class HeartbeatSender extends Thread {
+        public void run() {
+            while (running) {
+                String msg = "HEARTBEAT:" + pid;
+
+                for (Map.Entry<Integer, String> entry : processTable.entrySet()) {
+                    int targetPid = entry.getKey();
+                    if (targetPid == pid) continue;
+
+                    String[] hostPort = entry.getValue().split(":");
+                    String host = hostPort[0];
+                    int targetPort = Integer.parseInt(hostPort[1]);
+
+                    try (Socket socket = new Socket(host, targetPort);
+                         PrintWriter out = new PrintWriter(socket.getOutputStream(), true)) {
+                        out.println(msg);
+                        System.out.println("[P" + pid + "] send heartbeat to P" + targetPid);
+                    } catch (IOException e) {
+                        System.out.println("[P" + pid + "] failed to send heartbeat to P" + targetPid);
+                    }
+                }
+
+                System.out.println("Sent HEARTBEAT from P" + pid);
+
+                try {
+                    Thread.sleep(1000); // ทุก 1 วินาที
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    break;
+                }
+            }
+        }
+    }
